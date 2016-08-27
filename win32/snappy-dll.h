fa5fc90c3dc584050b809eff7414576fb178afca
Kelvin Lee <kiyolee@hotmail.com>
2016-08-27 17:17:16 +1000

    Use static library as default.

--- win32/snappy-dll.h
+++ win32/snappy-dll.h
@@ -1,12 +1,14 @@
 #ifndef UTIL_SNAPPY_SNAPPY_DLL_H_
 #define UTIL_SNAPPY_SNAPPY_DLL_H_
 
-#if defined(SNAPPY_STATIC)
-#define __SNAPPY_API
-#elif defined(SNAPPY_DLL)
+#if defined(SNAPPY_DLL) && !defined(SNAPPY_STATIC)
+#ifdef BUILDING_SNAPPY
 #define __SNAPPY_API __declspec(dllexport)
 #else
 #define __SNAPPY_API __declspec(dllimport)
+#endif
+#else
+#define __SNAPPY_API
 #endif
 
 #endif  // UTIL_SNAPPY_SNAPPY_DLL_H_
